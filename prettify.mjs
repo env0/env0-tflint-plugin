b96e9f3a8c592834272ea7ed639c6e13e560def3
ChaimP <chaim.platonov@env0.com>
2024-03-04 14:51:09 +0200

    pretty output

--- prettify.mjs
+++ prettify.mjs
@@ -1,4 +1,4 @@
-import { readFile, writeFile } from 'node:fs/promises';
+import {readFile, writeFile} from 'node:fs/promises';
 import process from 'node:process';
 
 const colorCode = {
@@ -7,6 +7,12 @@ const colorCode = {
     notice: '\x1b[90m',
     reset: '\x1b[0m',
 };
+
+const readFileLines = (fileName, startLine, endLine) => {
+    return readFile(fileName, 'utf-8')
+        .then((content) => content.split('\n').slice(startLine - 1, endLine).join('\n'))
+        .catch(() => '');
+}
 
 (async () => {
     const tfLintJson = JSON.parse(await readFile('tflint.json', 'utf-8'));
@@ -23,12 +29,24 @@ const colorCode = {
 
     await writeFile(process.env.ENV0_STEP_SUMMARY, summary);
 
-    const issues = tfLintJson.issues.map((issue) => {
-        const {rule: {severity, link}, message, range: {filename, start: {line: start}, end: {line: end}}} = issue;
+    const issuesWithContent = await Promise.all(tfLintJson.issues.map(async (issue) => {
+        if (issue.range.filename) {
+            issue.content = await readFileLines(issue.range.filename, issue.range.start.line, issue.range.end.line);
+        }
+        return issue;
+    }));
+
+    const issues = issuesWithContent.map((issue) => {
+        const {
+            rule: {severity, link},
+            message,
+            range: {filename, start: {line: start}, end: {line: end}},
+            content
+        } = issue;
         return [
             `- ${severity}: [${message}](${link})`,
-            filename ? `  - File: ${filename} - ` : undefined,
-            filename ? (start === end ? `  - Line ${start}` : `  - Lines ${start} - ${end}`) : undefined,
+            filename ? `  - File \`${filename}\` - ` + (start === end ? `line ${start}` : `lines ${start} - ${end}`) : undefined,
+            content ? '```hcl\n' + content + '\n```' : undefined,
         ]
             .filter(l => l)
             .join('\n');
